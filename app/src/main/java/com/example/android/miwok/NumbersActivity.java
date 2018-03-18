1b1263ba35c6870564c40b5b4ff72b5a853748e0
eslam faisal <eslamfaisal@github.com>
2018-03-18 06:27:47 +0200

    make list with array adapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,14 +1,14 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +20,27 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        List<String> words = new ArrayList<>();
+        final List<String> words = new ArrayList<>();
         words.add("eslam");
         words.add("faisal");
         Log.i("NumbersActivity",""+words.get(0));
-        ListView linearLayout = (ListView) findViewById(R.id.my_List);
+        GridView linearLayout = (GridView) findViewById(R.id.my_List);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (this,android.R.layout.simple_list_item_1,words);
         linearLayout.setAdapter(adapter);
+        linearLayout.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+               String s = words.get(i);
+                Toast.makeText(NumbersActivity.this,s,Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
